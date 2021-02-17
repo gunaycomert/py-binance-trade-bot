8b4dcc416b6df6dd849052690104a82084e00793
gunaycomert <75781311+gunaycomert@users.noreply.github.com>
2021-02-17 16:59:53 +0300

    Update binance.py

--- binance.py
+++ binance.py
@@ -1,11 +1,28 @@
 from binance.client import Client
+import pandas as pd
+from colorama import init
+from decimal import *
 import time
 import requests
+import os
 
-binance = Client('api_', 'secret_')
-gt = binance.get_server_time()
-
+init()
+clear = lambda: os.system('cls')
+binance = Client('api_key', 'secret')
 tumFiyatlar = binance.get_all_tickers()
+coinler = []
+free_ = []
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def fiyatBul(sym):
     count = 0
@@ -16,54 +33,76 @@ def fiyatBul(sym):
             val = i.get('price')
             count = count-1
             return count
-
-def hesapla(anlıkFiyat, alımFiyat):
-    degisim = ((anlıkFiyat - alımFiyat) / anlıkFiyat) * 100
-    return degisim
-
+            
 def do():
-    
     coinler = []
+    free_ = []
     coinGetir = binance.get_account()
-    
     for i in range(0, len(coinGetir['balances'])): 
-        adet = float(coinGetir['balances'][i]['free'])
-        if(adet > 1):
-            coinler.append(coinGetir['balances'][i]['asset']+'USDT')
+        if(Decimal(coinGetir['balances'][i]['free']) > 1):
+            if(coinGetir['balances'][i]['asset'] != 'USDT'):
+                coinler.append(coinGetir['balances'][i]['asset']+'USDT')
+                free_.append(coinGetir['balances'][i]['free'])
 
-    coinler.remove('USDTUSDT')
+    for i in range(0, len(coinler)):
+        orderS = binance.get_all_orders(symbol=coinler[i], limit=1)
+        if(orderS[0]['type'] == 'MARKET' and  orderS[0]['side'] == 'BUY'):
+            alınanMiktar = Decimal(orderS[0]['executedQty'])
+            toplamFiyat = Decimal(orderS[0]['cummulativeQuoteQty'])
+            alımFiyatı = Decimal(orderS[0]['cummulativeQuoteQty']) / Decimal(orderS[0]['executedQty'])
 
-    for i in range(0, len(coinler)): 
+            anlıkFiyat = Decimal(tumFiyatlar[fiyatBul(coinler[i])].get('price'))
+            eldekiAdet = Decimal(free_[i])
 
-        alınanlar = binance.get_all_orders(symbol=coinler[i], limit=1)
-        adet = float(alınanlar[0]['executedQty'])
-        fiyat = float(alınanlar[0]['cummulativeQuoteQty']) / adet
+            toplamFiyatAnlık = anlıkFiyat * eldekiAdet
 
-        coinFiyat = tumFiyatlar[fiyatBul(coinler[i])].get('price')
+            if(toplamFiyatAnlık > toplamFiyat):
+                yuzdeDegisim = Decimal(((anlıkFiyat - alımFiyatı) / alımFiyatı) * 100)
+                fiyatDeğişimi = Decimal(toplamFiyatAnlık - toplamFiyat)
+                yuzdeDegisimDuzeltilsim = Decimal(yuzdeDegisim).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+                fiyatDegisimDuzeltilsim = Decimal(fiyatDeğişimi).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+                if(yuzdeDegisimDuzeltilsim > 6):
+                    minSatmaOranı_ = binance.get_symbol_info(orderS[0]['symbol'])
+                    minSatmaOranı = Decimal(minSatmaOranı_['filters'][2]['minQty'])
+                    if(minSatmaOranı == Decimal("1.00000000")):
+                        satılacakMiktar=int(eldekiAdet)
+                    if(minSatmaOranı == Decimal("0.10000000")):
+                        satılacakMiktar=Decimal(eldekiAdet).quantize(Decimal('.1'), rounding=ROUND_DOWN)
+                    if(minSatmaOranı == Decimal("0.01000000")):
+                        satılacakMiktar=Decimal(eldekiAdet).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+                    if(minSatmaOranı == Decimal("0.00100000")):
+                        satılacakMiktar = Decimal(eldekiAdet).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+                    if(minSatmaOranı == Decimal("0.00010000")):
+                        satılacakMiktar=Decimal(eldekiAdet).quantize(Decimal('.0001'), rounding=ROUND_DOWN)
+                    if(minSatmaOranı == Decimal("0.00001000")):
+                        satılacakMiktar=Decimal(eldekiAdet).quantize(Decimal('.00001'), rounding=ROUND_DOWN)
+                    if(minSatmaOranı == Decimal("0.00000100")):
+                        satılacakMiktar=Decimal(eldekiAdet).quantize(Decimal('.000001'), rounding=ROUND_DOWN)
+                    order = binance.order_market_sell(symbol=orderS[0]['symbol'],quantity=satılacakMiktar)
+                    send_text = 'https://api.telegram.org/bot'+bot_id+'/sendMessage?chat_id='+chat_id+'&text='+orderS[0]['symbol']+' '+str(fiyatDegisimDuzeltilsim)
+                    response = requests.get(send_text)
+                print(bcolors.WARNING + orderS[0]['symbol'] + bcolors.ENDC+' '+bcolors.OKGREEN +'%'+ str(yuzdeDegisimDuzeltilsim) + bcolors.ENDC+' '+bcolors.OKGREEN + str(fiyatDegisimDuzeltilsim) + bcolors.ENDC)
 
-        fiyatDegisim_ = hesapla(float(coinFiyat),fiyat)
-        fiyatDegisim = '% ' +str(float("{:.3f}".format(fiyatDegisim_)))
-
-        almaFiyatı = float(alınanlar[0]['cummulativeQuoteQty'])
-        anlıkFiyat = float(adet * float(coinFiyat))
-
-        sonuc_ = anlıkFiyat-almaFiyatı
-        sonuc = str(float("{:.3f}".format(sonuc_)))
-
-        if(almaFiyatı > anlıkFiyat):
-            sonuc = sonuc + ' dolar zarar'
-        if(anlıkFiyat > almaFiyatı):
-            sonuc = sonuc + ' dolar kar'
-        if(fiyatDegisim_ < -5):
-            sonuc = 'dikkatli ol :)'
-
-        send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+chat_id+'&text='+ coinler[i] +' -> '+ fiyatDegisim + ' -> '+sonuc
-        response = requests.get(send_text)
-        print(almaFiyatı)
-        print(anlıkFiyat)
+            if(toplamFiyat > toplamFiyatAnlık):
+                yuzdeDegisim = Decimal(((anlıkFiyat - alımFiyatı) / alımFiyatı) * 100)
+                fiyatDeğişimi = Decimal(toplamFiyatAnlık - toplamFiyat)
+                yuzdeDegisimDuzeltilsim = Decimal(yuzdeDegisim).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+                fiyatDegisimDuzeltilsim = Decimal(fiyatDeğişimi).quantize(Decimal('.001'), rounding=ROUND_DOWN)
+                print(bcolors.WARNING + orderS[0]['symbol'] + bcolors.ENDC+' '+bcolors.FAIL +'%'+ str(yuzdeDegisimDuzeltilsim) + bcolors.ENDC+' '+bcolors.FAIL + str(fiyatDegisimDuzeltilsim) +bcolors.ENDC)
+        #time.sleep(1)
+                
 
 if __name__ == "__main__":
     while True:
-        tumFiyatlar = binance.get_all_tickers()
-        do()
-        time.sleep(62)
+        try:
+            for i in range(5):
+                i += 1
+                tumFiyatlar = binance.get_all_tickers()
+                do()
+                if(i == 5):
+                    clear()
+                    i = 0
+        except:
+            print("timeout") 
+            pass
+            
